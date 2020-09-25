732c0262896c37ad451b3b14aa88141c4bf9d551
Junior <jjalves96@gmail.com>
2020-09-25 07:24:13 -0300

    fix error styled-components

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -13,4 +13,10 @@ describe('<Main />', () => {
     // Snapshot
     expect(container.firstChild).toMatchSnapshot();
   });
+
+  // it('should render the colors correctly', () => {
+  //   const { container } = render(<Main />);
+
+  //   expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
+  // });
 });
